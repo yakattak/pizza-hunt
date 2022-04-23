ecc04bf424b8ec5b9efb901d50684ea702410f80
yakattak <stevecalebrese@gmail.com>
2022-04-23 16:44:14 -0400

    somewhere in 12.2.5

---
+++ models/Pizza.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],
+
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+
+      ref: 'Comment'
+    }
+  ]
+},
+
+{
+  toJSON: {
+  virtuals: true,
+},
+id: false
+}
+);
+
+
+//get total count of comments
+
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const Pizza = require('./Pizza');
+
+const Comment = require('./Comment');
+
+module.exports = { Pizza };
+
+
