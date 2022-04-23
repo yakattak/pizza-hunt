93d867e82db83146a6772c35058baf32b20a42ec
yakattak <stevecalebrese@gmail.com>
2022-04-23 17:46:46 -0400

    .2

--- models/Comment.js
+++ models/Comment.js
@@ -1,20 +1,17 @@
 const { Schema, model } = require('mongoose');
 
-const CommentSchema = new Schema ({
-    writtenBy: {
-        type: String
-    },
-
-    commentBody : {
-        type: String
-    },
-
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-})
-
+const CommentSchema = new Schema({
+  writtenBy: {
+    type: String
+  },
+  commentBody: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const Comment = model('Comment', CommentSchema);
 
