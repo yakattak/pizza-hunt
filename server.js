312a919ba40016ee0aa489503eb8bf59e55232a7
yakattak <stevecalebrese@gmail.com>
2022-04-25 17:58:16 -0400

    removed useFindAndModify which says not supported

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-  useFindAndModify: false,
+app.use(require('./routes'));
+
+//connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
-app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
